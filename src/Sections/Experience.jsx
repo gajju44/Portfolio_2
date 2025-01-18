f67924cb25189f3e607d43fba63260eb17dbcf77
gajj44 <gajjunaphade@gmail.com>
2025-01-18 14:15:02 +0530

    added navigation for experience

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -64,7 +64,7 @@ useEffect(() => {
 
   return (
    <>
-  <div className="w-screen min-h-screen pb-32 px-5 sm:px-12 lg:px-32 xl:px-40">
+  <div id='Experience' className="w-screen min-h-screen pb-32 px-5 sm:px-12 lg:px-32 xl:px-40">
         <div className="flex flex-col xl:mx-[130px] gap-10">
           <Heading Number="2" Heading="Where I’ve Worked" />
 
